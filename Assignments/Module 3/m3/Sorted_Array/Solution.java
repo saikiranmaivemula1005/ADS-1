4e46f5eef8017ba66b0516dc334dfa4e61b52ec4
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-26 20:30:35 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- Assignments/Module 3/m3/Sorted_Array/Solution.java
+++ Assignments/Module 3/m3/Sorted_Array/Solution.java
@@ -5,6 +5,7 @@ class Solution {
 		while (sc.hasNext()){
 			int size1 = sc.nextInt();
 			int size2 = sc.nextInt();
+			sc.nextLine();
 			String[] firstArray = sc.nextLine().split(",");
 			String[] secondArray = sc.nextLine().split(",");
 			System.out.println(Arrays.toString(firstArray));
