9e7c45379b28145c596978e134c78db5af2ea20b
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-26 19:51:56 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- Assignments/Module 3/m3/3-sum/Solution.java
+++ Assignments/Module 3/m3/3-sum/Solution.java
@@ -25,8 +25,7 @@ class Solution {
 		Arrays.sort(array);
 		for (int i = 0; i < length && array[i] < 0; i++) {
 			for (int j = i + 1; j  < length && array[i] + array[j] < 0; j++) {
-				//int k = Arrays.binarySearch(array, j + 1, length-1, -array[i] - array[j]);
-				int k = b.binarySearch(array, j + 1, length - 1, -array[i] - array[j]);
+				int k = b.binarySearch(array, j + 1, length-1, -array[i] - array[j]);
 				if (k > j) {
 					count++;
 				}
