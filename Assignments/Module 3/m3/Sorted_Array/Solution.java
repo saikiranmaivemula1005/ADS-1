643daa9ab1a6425ed87b4d4c5040fa2848c52c55
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-26 20:27:07 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- Assignments/Module 3/m3/Sorted_Array/Solution.java
+++ Assignments/Module 3/m3/Sorted_Array/Solution.java
@@ -2,16 +2,14 @@ import java.util.*;
 class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
-		int[] firstArray = new int[size];
-		int[] secondArray = new int[size];
-		for (int i = 0; i < firstArray.length; i++) {
-			firstArray[i] = sc.nextInt();
+		while (sc.hasNext()){
+			int size1 = sc.nextInt();
+			int size2 = sc.nextInt();
+			String[] firstArray = sc.nextLine().split(",");
+			String[] secondArray = sc.nextLine().split(",");
+			System.out.println(Arrays.toString(firstArray));
+			System.out.println(Arrays.toString(secondArray));
 		}
-		for (int i = 0; i < secondArray.length; i++) {
-			secondArray[i] = sc.nextInt();
-		}
-		System.out.println(Arrays.toString(firstArray));
-		System.out.println(Arrays.toString(secondArray));
+
 	}
 }
