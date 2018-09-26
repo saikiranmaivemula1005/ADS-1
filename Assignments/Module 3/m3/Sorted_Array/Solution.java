a4864b505e2f877b4eb760734d70fde15930fb16
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-26 20:38:17 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- Assignments/Module 3/m3/Sorted_Array/Solution.java
+++ Assignments/Module 3/m3/Sorted_Array/Solution.java
@@ -8,14 +8,16 @@ class Solution {
 			sc.nextLine();
 			String[] firstArray = sc.nextLine().split(",");
 			String[] secondArray = sc.nextLine().split(",");
-			System.out.println(Arrays.toString(firstArray));
-			System.out.println(Arrays.toString(secondArray));
+			// System.out.println(Arrays.toString(firstArray));
+			// System.out.println(Arrays.toString(secondArray));
+			int variable = 0;
 			String[] outputArray = new String[size1 + size2];
 			for (int i = 0; i < size1; i++) {
 				outputArray[i] = firstArray[i];
+				variable = i;
 			}
 			for (int j = 0; j < size2; j++) {
-				outputArray[j] = secondArray[j];
+				outputArray[variable + 1 ] = secondArray[j];
 			}
 			System.out.println(Arrays.toString(outputArray));
 		}
