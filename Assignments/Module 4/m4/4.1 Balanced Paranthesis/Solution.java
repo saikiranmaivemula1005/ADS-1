b63cc8e358f77b755d34bc34d8c65429e5789f18
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-27 19:15:44 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Assignments/Module 4/m4/4.1 Balanced Paranthesis/Solution.java
+++ Assignments/Module 4/m4/4.1 Balanced Paranthesis/Solution.java
@@ -48,59 +48,7 @@ class Stack {
         return (top == -1);
     }
 }
-/**
- * Class for balanced paranthesis.
- */
-class balancedParanthesis {
-	/**
-	 * constructor for the class.
-	 */
-	protected balancedParanthesis() {
 
-	}
-    /**
-     * Checks if matching pair exist or not.
-     * @param      inputOne  first string
-     * @param      inputTwo  second string
-     * @return     True if matching pair exist.
-     */
-    boolean isMatching(final String inputOne, final String inputTwo) {
-        if (inputOne.equals("(") && inputTwo.equals(")")) {
-            return true;
-        }
-        if (inputOne.equals("{") && inputTwo.equals("}")) {
-            return true;
-        }
-        if (inputOne.equals("[") && inputTwo.equals("]")) {
-            return true;
-        }
-        return false;
-    }
-    /**
-     * checkBalanced method.
-     * @param      array   The array
-     * @return     True if balanced, False otherwise.
-     */
-    boolean checkBalanced(final String[] array) {
-        Stack stack = new Stack(array.length);
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].equals("{")
-                || array[i].equals("(") || array[i].equals("[")) {
-                stack.push(array[i]);
-            }
-            if (array[i].equals("}")
-                || array[i].equals(")") || array[i].equals("]")) {
-                if (stack.isStackEmpty()) {
-                    return false;
-                }
-                if (!isMatching(stack.pop(), array[i])) {
-                    return false;
-                }
-            }
-        }
-        return stack.isStackEmpty();
-    }
-}
 /**
  * Class for solution.
  */
