7a37672e5e29f7d15b68f10ec97339743a2e9ad1
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-27 13:55:34 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ Assignments/Module 4/m4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numberOfLines = sc.nextInt();
+		while (sc.hasNext()) {
+			String line = sc.next();
+			checkbalanced(line);
+		}
+	}
+	public static String checkbalanced(String input) {
+		return input;
+	}
+}
