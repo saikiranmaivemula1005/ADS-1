fa05f36dac06118328a22f9814b6e488f070ff31
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-27 13:56:40 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- Assignments/Module 4/m4/4.1 Balanced Paranthesis/Solution.java
+++ Assignments/Module 4/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,12 +1,24 @@
 import java.io.*;
 import java.util.*;
+/**
+ * Class for solution.
+ */
 class Solution {
+	/**
+	 * Constructor for solution class.
+	 */
 	Solution() {
 
 	}
-	public static void main(String[] args) {
+	/**
+	 * main method.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int numberOfLines = sc.nextInt();
+		System.out.println(numberOfLines);
 		while (sc.hasNext()) {
 			String line = sc.next();
 			checkbalanced(line);
