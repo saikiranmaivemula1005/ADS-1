fcd3e0f45b7f3ce0f1abe8cc6ed4bbcdd35cf19a
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-27 19:01:49 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Assignments/Module 4/m4/4.1 Balanced Paranthesis/Solution.java
+++ Assignments/Module 4/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,49 +1,131 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+/**
+ * Stack class to store the elements.
+ */
+class Stack {
+    /**
+     * top variable.
+     */
+    private int top;
+    /**
+     * array of type String array to store the elements.
+     */
+    private String[] array;
+    /**
+     * n to store the size.
+     */
+    private int n;
+    /**
+     * Constructor for stack class.
+     * @param      size  The size
+     */
+    Stack(final int size) {
+        n = size;
+        top = -1;
+        array = new String[n];
+    }
+    /**
+     * push method which pushes the element into stack.
+     * @param      x     element to be pushed.
+     */
+    void push(final String x) {
+        array[++top] = x;
+    }
+    /**
+     * pop method which pops the element from stack.
+     * @return     element which is popped.
+     */
+    String pop() {
+        String ele = array[top];
+        top--;
+        return ele;
+    }
+    /**
+     * Determines if empty.
+     * @return     True if empty, False otherwise.
+     */
+    boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        }
+        return false;
+    }
+}
+/**
+ * Class for is balanced.
+ */
+class IsBalanced {
+    /**
+     * Determines if matching pair.
+     * @param      str1  The string 1
+     * @param      str2  The string 2
+     * @return     True if matching pair, False otherwise.
+     */
+    boolean isMatchingPair(final String str1, final String str2) {
+        if (str1.equals("(") && str2.equals(")")) {
+            return true;
+        }
+        if (str1.equals("{") && str2.equals("}")) {
+            return true;
+        }
+        if (str1.equals("[") && str2.equals("]")) {
+            return true;
+        }
+        return false;
+    }
+    /**
+     * areBalanced method to check
+       whether the parenthesis are balanced or not.
+     * @param      array   The array
+     * @return     True if balanced, False otherwise.
+     */
+    boolean areBalanced(final String[] array) {
+        Stack st = new Stack(array.length);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals("{")
+                || array[i].equals("(") || array[i].equals("[")) {
+                st.push(array[i]);
+            }
+            if (array[i].equals("}")
+                || array[i].equals(")") || array[i].equals("]")) {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                if (!isMatchingPair(st.pop(), array[i])) {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructor for solution class.
-	 */
-	Solution() {
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int numberOfLines = sc.nextInt();
-		// System.out.println(numberOfLines);
-		while (sc.hasNext()) {
-			String line = sc.next();
-			Node n = new Node(line);
-			if (n.checkbalanced(line)) {
-				System.out.println("YES");
-			}
-			else {
-				System.out.println("NO");
-			}
-		}
-	}
-}
-class Node {
-	String item;
-	Node next;
-	Node(String value) {
-		item = value;
-		next = null;
-	}
-	public boolean checkbalanced(String input) {
-		// System.out.println(input);
-		String[] tokens = input.split("");
-		System.out.println(Arrays.toString(tokens));
-		return (next == null);
-	}
-
-
+    }
+    /**
+     * main method to drive the function.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int noofinputs = Integer.parseInt(sc.nextLine());
+        while (noofinputs != 0) {
+            String line = sc.nextLine();
+            String[] array = line.split("");
+            IsBalanced b = new IsBalanced();
+            if (b.areBalanced(array)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+            noofinputs--;
+        }
+    }
 }
