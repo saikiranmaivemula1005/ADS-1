1c06ec5d779694a1972d051da906ed36523cff2d
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-29 12:03:17 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- Assignments/Module 6/m6/Assignment-1/Solution.java
+++ Assignments/Module 6/m6/Assignment-1/Solution.java
@@ -17,7 +17,7 @@ class LinkedList {
      *
      * @param      data  The data
      */
-    void insert(String data) {
+    void insert(final String data) {
         Node node = new Node();
         node.data = data;
         node.next = null;
@@ -58,7 +58,7 @@ class LinkedList {
      *
      * @return returns the value at that node.
      */
-    String get(int index) {
+    String get(final int index) {
         Node node = head;
         String s = "";
         node = node.next;
@@ -75,16 +75,32 @@ class LinkedList {
         return s;
     }
 }
+/**
+ * class for stack.
+ */
 class Stack{
     String[] array;
     int n;
+    /**
+     * Constructor for stack class.
+     */
     Stack() {
         array = new String[100000000];
         n = 0;
     }
-    void push(String item) {
+    /**
+     * method to push elements into stack.
+     *
+     * @param      item  string item.
+     */
+    void push(final String item) {
         array[n++] = item;
     }
+    /**
+     * method to remove elements from stack.
+     *
+     * @return returns the removed element.
+     */
     String pop() {
         if (isEmpty()) {
             return null;
@@ -92,19 +108,46 @@ class Stack{
         String s = array[--n];
         return s;
     }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
     boolean isEmpty() {
         return (n==0);
     }
+    /**
+     * returns size of stack.
+     *
+     * @return returns size of stack.
+     */
     int size() {
         return n;
     }
-    String get(int i) {
+    /**
+     * returns element at the given index.
+     *
+     * @param      i  integer parameter.
+     *
+     * @return returns element at the given index
+     */
+    String get(final int i) {
         return array[i];
     }
 }
+/**
+ * Class for add large numbers.
+ */
 class AddLargeNumbers {
+    /**
+     * convert number to digits.
+     *
+     * @param      number  string variable
+     *
+     * @return  returns the list of digits.
+     */
     
-    public static LinkedList numberToDigits(String number) {
+    public static LinkedList numberToDigits(final String number) {
         Node node = new Node();
         LinkedList list = new LinkedList();
         String[] tokens = number.split("");
@@ -113,9 +156,15 @@ class AddLargeNumbers {
         }
         return list;
     }
+    /**
+     * convert the digits to number.
+     *
+     * @param      list  linked list type list.
+     *
+     * @return  returns the linked list.
+     */
 
-
-    public static String digitsToNumber(LinkedList list) {
+    public static String digitsToNumber(final LinkedList list) {
         String s = "";
         Node n = new Node();
         list.show();
@@ -125,8 +174,15 @@ class AddLargeNumbers {
 
         return s;
     }
-
-    public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
+    /**
+     * method to Add large numbers.
+     *
+     * @param      list1  The list 1
+     * @param      list2  The list 2
+     *
+     * @return returns the addition of given numbers.
+     */
+    public static LinkedList addLargeNumbers(final LinkedList list1, final LinkedList list2) {
         LinkedList outputList = new LinkedList();
         int outputListsize = 0;
         Stack s1 = new Stack();
@@ -154,14 +210,27 @@ class AddLargeNumbers {
         return outputList;
     }
 }
-
+/**
+ * Class for solution.
+ */
 public class Solution {
-    public static void main(String[] args) {
+    /**
+     * constructor for solution class.
+     */
+    Solution() {
+
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         String p = sc.nextLine();
         String q = sc.nextLine();
-        switch(input){
+        switch (input){
             case "numberToDigits":
                 LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
                 LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
