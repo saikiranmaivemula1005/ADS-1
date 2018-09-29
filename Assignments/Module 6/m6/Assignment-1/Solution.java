08e87638bd3282c0c08ec22599db61eb47d67073
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-09-29 21:58:05 +0530

    "check_style_score -> 12 of 12 passed. style score: 0/1 "

--- Assignments/Module 6/m6/Assignment-1/Solution.java
+++ Assignments/Module 6/m6/Assignment-1/Solution.java
@@ -1,225 +1,67 @@
 import java.util.Scanner;
-/**
- * class for linkedist.
- */
-class LinkedList {
-    /**
-     * head of node type.
-     */
-    private Node head;
-    /**
-     * count of integer type.
-     */
-    private int count = 0;
-    /**
-     * method to insert data.
-     *
-     * @param      data  The data
-     */
-    void insert(final String data) {
-        Node node = new Node();
-        node.data = data;
-        node.next = null;
-        if (head == null) {
-            head = node;
-        } else {
-            Node n = head;
-            while (n.next != null) {
-                n = n.next;
-                count++;
-            }
-            n.next = node;
-        }
-    }
-    /**
-     * method to display elements.
-     */
-    void show() {
-        Node node = head;
-        while (node.next != null) {
-            System.out.print(node.data);
-            node = node.next;
-        }
-        System.out.print(node.data);
-    }
-    /**
-     * method to return size.
-     *
-     * @return returns size of linked list.
-     */
-    int size() {
-        return count;
-    }
-    /**
-     * method to get the element at that index.
-     *
-     * @param      index integer parameter
-     *
-     * @return returns the value at that node.
-     */
-    String get(final int index) {
-        Node x = head;
-       if (index < size() && index > 0) {
-        for (int i = 0; i < index; i++) {
-            x = x.next;
-        }
-        }
-        return x.data;
-    }
-}
-/**
- * class for stack.
- */
-class Stack {
-    /**
-     * array of string type.
-     */
-    private String[] array;
-    /**
-     * integer variable.
-     */
-    private int n;
-    /**
-     * Constructor for stack class.
-     */
-    Stack() {
-        final int variable = 100000000;
-        array = new String[variable];
-        n = 0;
-    }
-    /**
-     * method to push elements into stack.
-     *
-     * @param      item  string item.
-     */
-    void push(final String item) {
-        array[n++] = item;
-    }
-    /**
-     * method to remove elements from stack.
-     *
-     * @return returns the removed element.
-     */
-    String pop() {
-        if (isEmpty()) {
-            return null;
-        }
-        String s = array[--n];
-        return s;
-    }
-    /**
-     * Determines if empty.
-     *
-     * @return     True if empty, False otherwise.
-     */
-    boolean isEmpty() {
-        return (n == 0);
-    }
-    /**
-     * returns size of stack.
-     *
-     * @return returns size of stack.
-     */
-    int size() {
-        return n;
-    }
-    /**
-     * returns element at the given index.
-     *
-     * @param      i  integer parameter.
-     *
-     * @return returns element at the given index
-     */
-    String get(final int i) {
-        return array[i];
-    }
-}
-/**
- * Class for add large numbers.
- */
+import java.util.LinkedList;
+import java.util.Stack;
 class AddLargeNumbers {
-    /**
-     * constructor for the class.
-     */
-    protected AddLargeNumbers() {
-
-    }
-    /**
-     * convert number to digits.
-     *
-     * @param      number  string variable
-     *
-     * @return  returns the list of digits.
-     */
-    public static LinkedList numberToDigits(final String number) {
-        Node node = new Node();
-        LinkedList list = new LinkedList();
-        String[] tokens = number.split("");
-        for (int i = 0; i < number.length(); i++) {
-            list.insert(tokens[i]);
+   public static LinkedList numberToDigits(String number) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        char[] charA = number.toCharArray();
+        for (Character i : charA) {
+            list.add(Character.getNumericValue(i));
         }
         return list;
-    }
-    /**
-     * convert the digits to number.
-     *
-     * @param      list  linked list type list.
-     *
-     * @return  returns the linked list.
-     */
 
-    public static String digitsToNumber(final LinkedList list) {
-        String s = "";
-        Node n = new Node();
-        list.show();
-        while (n.next != null) {
-            s += n.data;
-        }
-
-        return s;
     }
-    /**
-     * method to Add large numbers.
-     *
-     * @param      list1  The list 1
-     * @param      list2  The list 2
-     *
-     * @return returns the addition of given numbers.
-     */
-    public static LinkedList addLargeNumbers(final LinkedList
-     list1, final LinkedList list2) {
-        LinkedList outputList = new LinkedList();
-        final int ten = 10;
-        String[] x = new String[ten];
-        int outputListsize = 0;
-        Stack s1 = new Stack();
-        Stack s2 = new Stack();
-        Stack s3 = new Stack();
-        int j = 0;
+
+    public static String digitsToNumber(LinkedList list) {
+        return list.toString().replaceAll("\\[", "").replaceAll("]", "").replaceAll(", ", "");
+    }
+
+    public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        Stack<Integer> stk1 = new Stack<Integer>();
+        Stack<Integer> stk2 = new Stack<Integer>();
+        Stack<Integer> stk3 = new Stack<Integer>();
+
         for (int i = 0; i < list1.size(); i++) {
-            s1.push(list1.get(i));
-            // System.out.print(list1.get(i) + " ");
-            j++;
+            int a = (Integer) (list1.get(i));
+            stk1.push(a);
         }
-        System.out.println();
         for (int i = 0; i < list2.size(); i++) {
-            s2.push(list2.get(i));
-            // System.out.print(list2.get(i) + " ");
-            j++;
+            int a = (Integer) (list2.get(i));
+            stk2.push(a);
         }
-        for (int i = 0; i < s1.size() + s2.size(); i++) {
-           System.out.println(s1.pop());
-            System.out.println(s2.pop());
+        int carry = 0;
+        final int ten = 10;
+        while (!stk1.isEmpty() && !stk2.isEmpty()) {
+            int a = stk1.pop() + stk2.pop() + carry;
+            stk3.push(a % ten);
+            carry = a / ten;
+
         }
-        // for (int i = 0; ) {
-        //     s3.push(s1.pop() + s2.pop());
-        //     x[i] = s3.get(i);
-        // }
-        // for (int i = 0; i < s1.size() + s2.size(); i++) {
-        //     outputList.insert(x[i]);
-        //     System.out.print(x[i] + " ");
-        // }
-        return outputList;
+        int l1 = list1.size();
+        int l2 = list2.size();
+        if (l2 < l1) {
+            int t = l1;
+            l1 = l2;
+            l2 = l1;
+            Stack<Integer> tstk = stk1;
+            stk2 = stk1;
+            stk1 = tstk;
+        }
+        if (l1 < l2) {
+            for (int i = l1; i < l2; i++) {
+                int a = stk2.pop() + carry;
+                stk3.push(a % ten);
+                carry = a / ten;
+            }
+        }
+        if (carry > 0) {
+            stk3.push(carry);
+        }
+        while (!stk3.isEmpty()) {
+            list.add(stk3.pop());
+        }
+        return list;
     }
 }
 /**
