c6f75488787eb8cdadf11c7ec9a8109b167cea9e
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-01 11:10:54 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- Assignments/Module7/m7/7.1 Steque/Solution.java
+++ Assignments/Module7/m7/7.1 Steque/Solution.java
@@ -18,11 +18,12 @@ class Steque {
 		if (size() == 0) {
 			System.out.println("Steque is empty.");
 		}
-		for (int i = 1; i < size - 1; i++) {
-			System.out.print(stack[i] + ", ");
-		}
-		System.out.print(stack[size - 1]);
-		System.out.println();
+		size--;
+		// for (int i = 1; i < size - 1; i++) {
+		// 	System.out.print(stack[i] + ", ");
+		// }
+		// System.out.print(stack[size - 1]);
+		// System.out.println();
 	}
 	int size() {
 		return size;
