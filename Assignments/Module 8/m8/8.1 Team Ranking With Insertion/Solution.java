a11d4cdb10075253354509da89a8da789f351d65
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-02 17:38:04 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

--- Assignments/Module 8/m8/8.1 Team Ranking With Insertion/Solution.java
+++ Assignments/Module 8/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,21 +1,49 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class InsertionSort {
-	void sort(String[] elements) {
+class Team {
+	String nameOfTeam;
+	int wins;
+	int losses;
+	int draws;
+	Team(String nameOfTeam, int wins, int losses, int draws) {
+		this.nameOfTeam = nameOfTeam;
+		this.wins = wins;
+		this.losses = losses;
+		this.draws = draws;
+	}
 
+}
+class InsertionSort {
+	void sort(int[] elementsArray) {
+		for (int i = 1; i < elementsArray.length; i++) {
+			int key = elementsArray[i];
+			int j = i - 1;
+			while (j > 0 && elementsArray[j] < key) {
+				elementsArray[j + 1] = elementsArray[j];
+				j = j - 1;
+			}
+		}
+		System.out.println(Arrays.toString(elementsArray));
 	}
 }
+
 class Solution {
+	// InsertionSort i = new InsertionSort();
 	public static void main(String[] args) {
-		InsertionSort isort = new InsertionSort();
 		Scanner sc = new Scanner(System.in);
-		String[] elements = new String[10];
-		for (int i = 0; i < 10; i++) {
-			String line = sc.next();
+		int count = 0;
+		while (sc.hasNext()) {
+			String line = sc.next(); 
 			String[] tokens = line.split(",");
-				elements[i] = (tokens[0]);
+			Team t = new Team(tokens[0], Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]));
+			// System.out.println(tokens[0]);
+			// System.out.println(tokens[1]);
+			// System.out.println(tokens[2]);
+			// System.out.println(tokens[3]);
+			// System.out.println("finish");
+			count++;
+
 		}
-		System.out.println(Arrays.toString(elements));
-		isort.sort(elements);
+		System.out.println(count);
 	}
 }
