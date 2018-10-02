61c5f328fbd93ba79632486844a6f2bbf5ad1e7e
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-02 10:13:47 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

--- Assignments/Module 8/m8/8.1 Team Ranking With Insertion/Solution.java
+++ Assignments/Module 8/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -11,12 +11,10 @@ class Solution {
 		Scanner sc = new Scanner(System.in);
 		String[] elements = new String[10];
 		for (int i = 0; i < 10; i++) {
-		while (sc.hasNext()) {
 			String line = sc.next();
 			String[] tokens = line.split(",");
 				elements[i] = (tokens[0]);
 		}
-	}
 		System.out.println(Arrays.toString(elements));
 		isort.sort(elements);
 	}
