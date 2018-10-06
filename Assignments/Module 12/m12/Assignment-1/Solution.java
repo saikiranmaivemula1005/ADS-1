ad8fe0ccb422a8980215a03a329a5230cc676699
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-06 12:23:59 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 12/m12/Assignment-1/Solution.java
+++ Assignments/Module 12/m12/Assignment-1/Solution.java
@@ -1,6 +1,209 @@
 import java.util.Scanner;
 import java.util.Comparator;
 /**
+ * Class for student.
+ */
+class Student {
+	/**
+	 * name variable.
+	 */
+	 String name;
+	 /**
+	  * age.
+	  */
+	 String dob;
+	 /**
+	  * marks variable.
+	  */
+	 int marks1;
+	 /**
+	  *  marks variable.
+	  */
+	 int marks2;
+	 /**
+	  *  marks variable.
+	  */
+	 int marks3;
+	 /**
+	  * total marks.
+	  */
+	 int total;
+	 /**
+	  * category.
+	  */
+	 String category;
+	 /**
+	  * Constructs the object.
+	  */
+	Student() {
+
+	}
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      name      The name
+	 * @param      dob       The dob
+	 * @param      marks1    The marks 1
+	 * @param      marks2    The marks 2
+	 * @param      marks3    The marks 3
+	 * @param      total     The total
+	 * @param      category  The category
+	 */
+	Student(String name, String dob, int marks1, int marks2, int marks3, int total, String category) {
+		this.name = name;
+		this.dob = dob;
+		this.marks1 = marks1;
+		this.marks2 = marks2;
+		this.marks3 = marks3;
+		this.total = total;
+		this.category = category;
+	}
+	/**
+	 * Gets the name.
+	 *
+	 * @return     The name.
+	 */
+	String getName() {
+		return name;
+	}
+	/**
+	 * gets age.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	String getdob() {
+		return dob;
+	}
+	/**
+	 * Gets the marks.
+	 *
+	 * @return     The marks.
+	 */
+	int getMarks() {
+		return total;
+	}
+	/**
+	 * Gets the category.
+	 *
+	 * @return     The category.
+	 */
+	String getCategory() {
+		return category;
+	}
+	/**
+	 * prints the data.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	String show() {
+		return name + "," + getMarks() + "," + getCategory();
+	}
+}
+/**
+ * Class for merge sort.
+ *
+ * @param      <T>   { parameter_description }
+ */
+class MergeSort<T> {
+    /**
+     * auxiliary array.
+     */
+    private  T[] aux;
+    /**
+     * sort method
+     *
+     * @param      a     { array }
+     * @param      cp    { comparator variable }
+     */
+    public  void sort(final T[] a, final Comparator<T> cp) {
+        aux = (T[]) new Object[a.length];
+        sort(a, 0, a.length - 1, cp);
+    }
+    /**
+     * merge method.
+     *
+     * @param      a     { array }
+     * @param      lo    The lower
+     * @param      mid   The middle
+     * @param      hi    The higher
+     * @param      cp    { comparator variable }
+     */
+    private void merge(final T[] a, final int lo, final int mid, final int hi, final Comparator<T> cp) {
+        int i = lo, j = mid + 1;
+        
+        for (int k = lo; k <= hi; k++)
+            aux[k] = a[k];
+
+        for (int k = lo; k <= hi; k++)
+            if      (i > mid)              a[k] = aux[j++];
+            else if (j > hi )              a[k] = aux[i++];
+            else if (less(aux[j], aux[i], cp)) a[k] = aux[j++];
+            else                           a[k] = aux[i++];
+    }
+    /**
+     * less method.
+     *
+     * @param      a     { variable}
+     * @param      b     { variable }
+     * @param      cp    {comparator variable }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private boolean less(T a, T b, Comparator<T> cp) {
+        return cp.compare(a,b) < 0;
+    }
+    /**
+     * sort method.
+     *
+     * @param      a     { array }
+     * @param      lo    The lower
+     * @param      hi    The higher
+     * @param      cp    { comparator variable}
+     */
+    private void sort(T[] a, int lo, int hi, Comparator<T> cp) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid, cp);
+        sort(a, mid + 1, hi, cp);
+        merge(a, lo, mid, hi, cp);
+    }
+}
+
+/**
+ * Class for allotment.
+ */
+class Allotment {
+	/**
+	 * students array.
+	 */
+	Student[] students;
+	// Student s = new Student();
+	MergeSort<Student> ms = new MergeSort<Student>();
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      students  The students
+	 */
+	Allotment(Student[] students) {
+		this.students = students;
+	}
+	void sortByMarks(final Comparator<Student> c) {
+		ms.sort(students, c);
+	}
+	void sortByAge(final Comparator<Student> c) {
+		ms.sort(students, c);
+	}
+	void showStudents() {
+		for (int i = students.length - 1; i >= 0; i--) {
+			System.out.println(students[i].show());
+		}
+	}
+	void sortByCategory(final Comparator<Student> c) {
+		ms.sort(students, c);
+	}
+
+}
+/**
  * Class for sort by marks.
  */
 class sortByMarks implements Comparator<Student> {
