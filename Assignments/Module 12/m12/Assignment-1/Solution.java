5db77cde59eeb8b22664445177def2cda1253dc0
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-06 13:20:47 +0530

    "ads1_m12_a1: 1/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 12/m12/Assignment-1/Solution.java
+++ Assignments/Module 12/m12/Assignment-1/Solution.java
@@ -281,6 +281,11 @@ class Solution {
         }
         for (int i = object.length - openSeats; i > 0; i--) {
             for (int j = 0; j < stseats; j++) {
+                if (object[i].getCategory().equals("BC")) {
+                    System.out.println(object[i].show());
+                }
+            }
+            for (int j = 0; j < stseats; j++) {
                 if (object[i].getCategory().equals("ST")) {
                     System.out.println(object[i].show());
                 }
