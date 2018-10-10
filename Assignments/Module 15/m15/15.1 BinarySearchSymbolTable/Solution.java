7c38bd1237a689c6ea8188d37ff9c95fd636c43c
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-10 15:27:23 +0530

    "ads1_m15_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/Module 15/m15/15.1 BinarySearchSymbolTable/Solution.java
@@ -0,0 +1,186 @@
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+/**
+ * Class for binary search st.
+ */
+class BinarySearchST<Key extends Comparable<Key>, Value>{
+	private Key[] keys;
+    private Value[] vals;
+    private int size = 0;
+	/**
+	 * constructor for the class.
+	 */
+	BinarySearchST() {
+
+	}
+	public int size() {
+		return size;
+	}
+	public Key max() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("called max() with empty symbol table");
+		}
+        return keys[size-1];
+	}
+	public Key floor(final Key key) {
+		 if (key == null) {
+		 	throw new IllegalArgumentException("argument to floor() is null");
+		 }
+        int i = rank(key);
+        if (i < size && key.compareTo(keys[i]) == 0) {
+        	return keys[i];
+        }
+        if (i == 0) {
+        	return null;
+        } else {
+        	return keys[i-1];
+        }
+	}
+	public boolean isEmpty() {
+        return size() == 0;
+    }
+	public int rank(final Key key) {
+		if (key == null) {
+			throw new IllegalArgumentException("argument to rank() is null");
+		}
+        int lo = 0, hi = size-1; 
+        while (lo <= hi) { 
+            int mid = lo + (hi - lo) / 2; 
+            int cmp = key.compareTo(keys[mid]);
+            if (cmp < 0) {
+            	hi = mid - 1;
+            } 
+            else if (cmp > 0) {
+            	lo = mid + 1;
+            } else {
+            	return mid;
+            } 
+        } 
+        return lo;
+	}
+	public void deleteMin(){
+		if (isEmpty()) {
+			throw new NoSuchElementException("Symbol table underflow error");
+		}
+        delete(min());
+	}
+	public void delete(Key key) {
+        if (key == null) {
+        	throw new IllegalArgumentException("argument to delete() is null");
+        } 
+        if (isEmpty()) {
+        	return;
+        }
+        int i = rank(key);
+        if (i == size || keys[i].compareTo(key) != 0) {
+            return;
+        }
+        for (int j = i; j < size-1; j++) {
+            keys[j] = keys[j+1];
+            vals[j] = vals[j+1];
+        }
+        size--;
+        keys[size] = null;
+        vals[size] = null;
+    }
+    public Key min() {
+        if (isEmpty()) {
+        	throw new NoSuchElementException("called min() with empty symbol table");
+        }
+        return keys[0]; 
+    }
+	public void put(Key key, Value value) {
+		if (key == null) {
+			throw new IllegalArgumentException("first argument to put() is null");
+		}
+        if (value == null) {
+            delete(key);
+            return;
+        }
+        int i = rank(key);
+        if (i < size && keys[i].compareTo(key) == 0) {
+            vals[i] = value;
+            return;
+        }
+	}
+	public boolean contains(final Key key) {
+		if (key == null) {
+			throw new IllegalArgumentException("argument to contains() is null");
+		}
+        return get(key) != null;
+	}
+	// public Iterable<Key> keys() {
+	// 	return keys(min(), max());
+	// }
+	//  public Iterable<Key> keys(Key lo, Key hi) {
+ //        if (lo == null) {
+ //        	throw new IllegalArgumentException("first argument to keys() is null");
+ //        } 
+ //        if (hi == null) {
+ //        	throw new IllegalArgumentException("second argument to keys() is null");
+ //        }
+ //        Queue<Key> queue = new Queue<Key>(); 
+ //        if (lo.compareTo(hi) > 0) {
+ //        	return queue;
+ //        }
+ //        for (int i = rank(lo); i < rank(hi); i++) {
+ //            queue.enqueue(keys[i]);
+ //        }
+ //        if (contains(hi)) {
+ //        	queue.enqueue(keys[rank(hi)]);
+ //        }
+ //        return queue; 
+ //    }
+	public Value get(final Key key) {
+		if (key == null) {
+			throw new IllegalArgumentException("argument to get() is null");
+		} 
+        if (isEmpty()) {
+        	return null;
+        }
+        int i = rank(key); 
+        if (i < size && keys[i].compareTo(key) == 0) {
+        	return vals[i];
+        }
+        return null;
+	}
+}
+
+class Solution {
+	protected Solution() {
+
+	}
+	public static void main(final String[] args) {
+		BinarySearchST<String, Integer> bst = new BinarySearchST<String, Integer>();
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			String inputWord = sc.next();
+			String[] tokens = inputWord.split(" ");
+			String line = sc.next();
+			String[] cases = line.split(" ");
+			switch (cases[0]) {
+				case "max":
+					bst.max();
+					break;
+				case "floor":
+					bst.floor(cases[1]);
+					break;
+				case "rank":
+					bst.rank(cases[1]);
+					break;
+				case "deleteMin":
+					bst.deleteMin();
+					break;
+				case "contains":
+					bst.contains(cases[1]);
+					break;
+				// case "keys":
+				// 	bst.keys();
+				// 	break;
+				case "get":
+					bst.get(cases[1]);
+					break;
+			}
+		}
+	}
+}
