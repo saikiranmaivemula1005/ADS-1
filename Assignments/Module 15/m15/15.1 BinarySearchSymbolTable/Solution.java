78b56725789ac56c8c0bd4124344ee2072c58531
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-10 18:31:14 +0530

    "ads1_m15_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 15/m15/15.1 BinarySearchSymbolTable/Solution.java
+++ Assignments/Module 15/m15/15.1 BinarySearchSymbolTable/Solution.java
@@ -11,8 +11,10 @@ class BinarySearchST<Key extends Comparable<Key>, Value>{
      * constructor for the class.
      */
     BinarySearchST() {
-        keys = (Key[]) new Comparable[size];
-        vals = (Value[]) new Object[size];
+    }
+    BinarySearchST(int capacity) {
+        keys = (Key[]) new Comparable[capacity];
+        vals = (Value[]) new Object[capacity];
     }
     public int size() {
         return size;
@@ -25,7 +27,7 @@ class BinarySearchST<Key extends Comparable<Key>, Value>{
     }
     public Key floor(final Key key) {
          if (key == null) {
-            throw new IllegalArgumentException("argument to floor() is null");
+            throw new IllegalArgumentException("argument to flowor() is null");
          }
         int i = rank(key);
         if (i < size && key.compareTo(keys[i]) == 0) {
@@ -44,25 +46,22 @@ class BinarySearchST<Key extends Comparable<Key>, Value>{
         if (key == null) {
             throw new IllegalArgumentException("argument to rank() is null");
         }
-        int lo = 0, hi = size - 1; 
-        while (lo <= hi) { 
-            int mid = lo + (hi - lo) / 2; 
+        int low = 0, high = size - 1; 
+        while (low <= high) { 
+            int mid = low + (high - low) / 2; 
             int cmp = key.compareTo(keys[mid]);
             if (cmp < 0) {
-                hi = mid - 1;
+                high = mid - 1;
             } 
             else if (cmp > 0) {
-                lo = mid + 1;
+                low = mid + 1;
             } else {
                 return mid;
             } 
         } 
-        return lo;
+        return low;
     }
     public void deleteMin(){
-        if (isEmpty()) {
-            throw new NoSuchElementException("Symbol table underflow error");
-        }
         delete(min());
     }
     public void delete(Key key) {
@@ -83,16 +82,27 @@ class BinarySearchST<Key extends Comparable<Key>, Value>{
         size--;
         keys[size] = null;
         vals[size] = null;
+         if (size > 0 && size == keys.length / 4) {
+            resize(keys.length / 2);
+        }
+    }
+     public void resize(int capacity) {
+        Key[] temporaykey = (Key[]) new Comparable[capacity];
+        Value[] temporarval = (Value[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            temporaykey[i] = keys[i];
+            temporarval[i] = vals[i];
+        }
+
+        vals = temporarval;
+        keys = temporaykey;
     }
     public Key min() {
-        if (isEmpty()) {
-            throw new NoSuchElementException("called min() with empty symbol table");
-        }
         return keys[0]; 
     }
     public void put(Key key, Value value) {
         if (key == null) {
-            throw new IllegalArgumentException("first argument to put() is null");
+            throw new IllegalArgumentException("Null key");
         }
         if (value == null) {
             delete(key);
@@ -103,21 +113,50 @@ class BinarySearchST<Key extends Comparable<Key>, Value>{
             vals[i] = value;
             return;
         }
+        if (size == keys.length) {
+            resize(2 * keys.length);
+        }
+        for (int j = size; j > i; j--)  {
+            keys[j] = keys[j-1];
+            vals[j] = vals[j-1];
+        }
+        keys[i] = key;
+        vals[i] = value;
+        size++;
     }
     public boolean contains(final Key key) {
-        if (key == null) {
-            throw new IllegalArgumentException("argument to contains() is null");
-        }
         return get(key) != null;
     }
-    public void keys() {
-        for (int i = 0; i < keys.length; i++) {
-            System.out.println(keys[i] + " " + vals[i]);
+    public List<String> keys() {
+        return keys(min(), max());
+    }
+    public List<String> keys(Key loww, Key highgh) {
+        if (loww == null) {
+            throw new IllegalArgumentException("first argument to keys() is null"); 
         }
+
+        if (highgh == null) {
+            throw new IllegalArgumentException("last argument to keys() is null"); 
+        }
+
+        List<String> list = new List<>();
+        if (loww.compareTo(highgh) > 0) {
+            return list;
+        }
+
+        for (int i = rank(loww); i < rank(highgh); i++) {
+            list.add((String)keys[i]);
+        }
+
+        if (contains(highgh)) {
+            list.add((String)keys[rank(highgh)]);
+        }
+
+        return list;
     }
     public Value get(final Key key) {
         if (key == null) {
-            throw new IllegalArgumentException("argument to get() is null");
+            throw new IllegalArgumentException("Key is null");
         } 
         if (isEmpty()) {
             return null;
@@ -137,35 +176,40 @@ class Solution {
     public static void main(final String[] args) {
         BinarySearchST<String, Integer> bst = new BinarySearchST<String, Integer>();
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
             String inputWord = sc.next();
             String[] tokens = inputWord.split(" ");
+            bst = new BinarySearchST<String, Integer>(tokens.length);
             for (int i = 0; i < tokens.length; i++) {
+                System.out.println(tokens[i]);
                 bst.put(tokens[i], i);
             }
+        while (sc.hasNext()) {
             String line = sc.next();
             String[] cases = line.split(" ");
             switch (cases[0]) {
                 case "max":
-                    bst.max();
+                    System.out.println(bst.max());
                     break;
                 case "floor":
-                    bst.floor(cases[1]);
+                    System.out.println(bst.floor(cases[1]));
                     break;
                 case "rank":
-                    bst.rank(cases[1]);
+                    System.out.println(bst.rank(cases[1]));
                     break;
                 case "deleteMin":
                     bst.deleteMin();
                     break;
                 case "contains":
-                    bst.contains(cases[1]);
+                    System.out.println(bst.contains(cases[1]));
                     break;
                 case "keys":
-                    bst.keys();
+                    List<String> list = bst.keys();
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println(list.get(i) + " " + bst.get(list.get(i)));
+                    }
                     break;
                 case "get":
-                    bst.get(cases[1]);
+                    System.out.println(bst.get(cases[1]));
                     break;
             }
         }
