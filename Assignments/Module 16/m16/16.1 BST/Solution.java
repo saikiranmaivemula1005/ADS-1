0c7cbd97a0cd7b7ee39fd47fc82406bb14e19e8b
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-11 11:02:48 +0530

    "ads1_m16_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 16/m16/16.1 BST/Solution.java
+++ Assignments/Module 16/m16/16.1 BST/Solution.java
@@ -18,25 +18,26 @@ class Book {
 		return this.price;
 	}
 }
-class Node {
-		 Book key;
-		 String value;
-		 Node left, right;
-		Node(Book k, String v) {
-			this.key = k;
-			this.value = v;
-		}
-	}
+
 
 class BinarySearchTree<Key extends Comparable<Key>, Value> {
 	BinarySearchTree() {
 
 	}
 	private Node root;
-	public void put(Book key, String value) {
+	private class Node {
+		private Book key;
+		private int value;
+		private Node left, right;
+		Node(Book k, int v) {
+			this.key = k;
+			this.value = v;
+		}
+	}
+	public void put(Book key, int value) {
 		 root = put(root, key, value);
 	}
-	private Node put(Node x, Book key, String value) {
+	private Node put(Node x, Book key, int value) {
 		if (x == null) {
 			return new Node(key, value);
 		}
@@ -51,7 +52,7 @@ class BinarySearchTree<Key extends Comparable<Key>, Value> {
 		}
 		return x;
 	}
-	public String get(Book key) {
+	public Integer get(Book key) {
 		Node x = root;
 		if (x == null) {
 			System.out.println("null");
@@ -83,7 +84,7 @@ class Solution {
 			switch (tokens[0]) {
 				case "put":
 					Book key = new Book(tokens[1], tokens[2], Float.parseFloat(tokens[3]));
-					String value = tokens[4];
+					int value = Integer.parseInt(tokens[4]);
 					bst.put(key, value);
 					break;
 				case "get":
