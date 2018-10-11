473d2c9b25d649c0e4698162b7838146423d2da9
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-11 10:34:49 +0530

    "ads1_m16_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/Module 16/m16/16.1 BST/Solution.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+class Book {
+	String name;
+	String author;
+	float price;
+	Book(String n, String a, float p) {
+		this.name = n;
+		this.author = a;
+		this.price = p;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public String getAuthor() {
+		return this.author;
+	}
+	public float getprice() {
+		return this.price;
+	}
+}
+
+
+class BinarySearchTree<Key extends Comparable<Key>, Value> {
+	BinarySearchTree() {
+
+	}
+	private Node root;
+	private class Node {
+		private Book key;
+		private int value;
+		private Node left, right;
+		Node(Book k, int v) {
+			this.key = k;
+			this.value = v;
+		}
+	}
+	public void put(Book key, int value) {
+		 root = put(root, key, value);
+	}
+	private Node put(Node x, Book key, int value) {
+		if (x == null) {
+			return new Node(key, value);
+		}
+		int cmp = key.getName().compareTo(x.key.getName());
+		if (cmp < 0) {
+			x.left = put(x.left, key, value);
+		}
+		if (cmp > 0) {
+			x.right = put(x.right, key, value);
+		} else {
+			x.value = value;
+		}
+		return x;
+	}
+	public int get(Book key) {
+		Node x = root;
+		while (x != null) {
+			int cmp = key.getName().compareTo(x.key.getName());
+				if (cmp < 0) {
+					x = x.left;
+				}
+				if (cmp > 0) {
+					x = x.right;
+				} else {
+					return x.value;
+				}
+			}
+			return 0;
+		}
+	}
+
+class Solution {
+	protected Solution() {
+
+	}
+	public static void main(String[] args) {
+		BinarySearchTree bst = new BinarySearchTree();
+		Scanner sc = new Scanner(System.in);	
+		while (sc.hasNext()) {
+			String[] tokens = sc.next().split(",");
+			switch (tokens[0]) {
+				case "put":
+					Book key = new Book(tokens[1], tokens[2], Float.parseFloat(tokens[3]));
+					int value = Integer.parseInt(tokens[4]);
+					bst.put(key, value);
+					break;
+				case "get":
+				    key = new Book(tokens[1], tokens[2], Float.parseFloat(tokens[3]));
+					System.out.println(bst.get(key));
+					break;
+				default:
+			}
+		}
+	}
+}
