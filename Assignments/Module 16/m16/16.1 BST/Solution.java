f2bfc25b2c895a6219c585a3d24fbbf37c414c0a
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-11 11:29:37 +0530

    "ads1_m16_a1: 1/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 16/m16/16.1 BST/Solution.java
+++ Assignments/Module 16/m16/16.1 BST/Solution.java
@@ -99,6 +99,9 @@ class BinarySearchTree<Key extends Comparable<Key>, Value> {
 	 */
 	public Integer get(Book key) {
 		Node x = root;
+		if (x == null) {
+			System.out.println("null");
+		}
 		while (x != null) {
 			int cmp = key.getName().compareTo(x.key.getName());
 				if (cmp < 0) {
