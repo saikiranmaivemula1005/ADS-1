a6cfa7488666928a57ae9f62e046f294b02d9425
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-13 12:29:24 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 18/m18/Week-3_Assignment-1/Solution.java
+++ Assignments/Module 18/m18/Week-3_Assignment-1/Solution.java
@@ -1,38 +1,32 @@
 import java.util.Scanner;
 class Stock {
 	String name;
-	String percentage;
-	Stock(String n, String p) {
+	float percentage;
+	Stock(String n, float p) {
 	this.name = n;
 	this.percentage = p;
 	}
 	String getName() {
 		return name;
 	}
-	String getPercentage() {
+	float getPercentage() {
 		return percentage;
 	}
 }
 
 class StockPercentage {
-	private MinPQ<String> minObj;
-    private MaxPQ<String> maxObj;
+	private MinPQ<Stock> minObj;
+    private MaxPQ<Stock> maxObj;
     StockPercentage() {
-    	minObj = new MinPQ<String>();
-		maxObj = new MaxPQ<String>();
+    	minObj = new MinPQ<Stock>();
+		maxObj = new MaxPQ<Stock>();
     }
     public void insert(Stock stock) {
-    	minObj.insert(stock.getPercentage());
-    	maxObj.insert(stock.getPercentage());
+    	minObj.insert(stock);
     }
-    public void printmin() {
+    public void print() {
     	for (int i = 0; i < 5; i++) {
     		minObj.get(i);
-    	}
-    }
-    public void printmax() {
-    	for (int  i = 0; i < 5; i++) {
-    		maxObj.get(i);
     	}
     }
 }
@@ -59,19 +53,14 @@ class Solution {
 		StockPercentage sp = new StockPercentage();
 		Scanner sc = new Scanner(System.in);
 		int numberOfStocks = sc.nextInt();
-		int x = 6;
-		while (sc.hasNextLine()) {
-				String data = sc.nextLine();
-				System.out.println(data);
-				String[] tokens = data.split(",");
-				String name = tokens[0];
-				System.out.println(tokens[0]);
-				String percentage = tokens[1];
-				System.out.println(tokens[1]);
-				Stock stock = new Stock(name, percentage);
-				sp.insert(stock);
+		for (int i = 0; i < numberOfStocks; i++) {
+			// String[] tokens = sc.nextLine().split(",");
+			String[] tokens = sc.nextLine().split(",");
+			String name = tokens[0];
+			float percentage = Float.parseFloat(tokens[1]);
+			Stock stock = new Stock(name, percentage);
+			sp.insert(stock);
 		}
-		sp.printmin();
-		sp.printmax();
+		sp.print();
 	}
 }
