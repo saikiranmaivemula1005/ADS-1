a389e430ac710907b37dc195eeb8ccbb7511f191
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-13 11:50:57 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 18/m18/Week-3_Assignment-1/Solution.java
+++ Assignments/Module 18/m18/Week-3_Assignment-1/Solution.java
@@ -62,10 +62,8 @@ class Solution {
 		int x = 6;
 		while (x != 0) {
 			for (int i = 0; i < numberOfStocks; i++) {
-				System.out.println(sc.nextLine());
 			String[] tokens = sc.nextLine().split(",");
 			String name = tokens[0];
-			System.out.println(tokens[0] + " " + tokens[1]);
 			float percentage = Float.parseFloat(tokens[1]);
 			Stock stock = new Stock(name, percentage);
 			sp.insert(stock);
