b448a8178b8cd25a8d9af22c8faf905cd7597e0a
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-25 12:07:29 +0530

    "ads1_m22_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/Module 22/m22/22.1 Ransom Note/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		String lineOne = sc.next();
+		String lineTwo = sc.next();
+	}
+}
