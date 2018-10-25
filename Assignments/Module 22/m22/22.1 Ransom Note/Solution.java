ff10bb495a9f3fa95abf5e20008307535c6c550e
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-25 16:32:09 +0530

    "ads1_m22_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 22/m22/22.1 Ransom Note/Solution.java
+++ Assignments/Module 22/m22/22.1 Ransom Note/Solution.java
@@ -1,18 +1,50 @@
 import java.util.Scanner;
+import java.util.LinkedList;
+ class SeparateChainingHashST<Key, Value> {
+ 	private int n, m;
+ 	private SequentialSearchST<Key, Value>[] st;
+ 	public SeparateChainingHashST() {
+
+    } 
+	 private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % m;
+    }
+     public void put(Key key, Value val) {
+        if (val == null) {
+            delete(key);
+            return;
+        }
+        int i = hash(key);
+        if (!st[i].contains(key)) n++;
+        st[i].put(key, val);
+    }
+    public void delete(Key key) {
+        int i = hash(key);
+        if (st[i].contains(key)) n--;
+        st[i].delete(key);
+    }
+}
 class Solution {
 	Solution() {
 
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		SeparateChainingHashST magazine = new SeparateChainingHashST();
+		SeparateChainingHashST ransom = new SeparateChainingHashST();
 		int m = sc.nextInt();
 		int n = sc.nextInt();
 		String lineOne = sc.next();
+		String[] mag = lineOne.split(" ");
 		String lineTwo = sc.next();
-		if (lineOne.equals(lineTwo)) {
-			System.out.println("Yes");
-		} else {
-			System.out.println("No");
+		String[] ran = lineTwo.split(" ");
+		for (int i = 0; i < lineOne.length(); i++) {
+			magazine.put(mag[i], i);
 		}
+		for (int i = 0; i < lineTwo.length(); i++) {
+			ransom.put(ran[i], i);
+		}
+		System.out.println(magazine);
+		System.out.println(ransom);
 	}
 }
