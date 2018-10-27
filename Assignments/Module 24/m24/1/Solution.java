ca75b19e6557f1b34d75d14aa62f48fd3c19e687
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-27 11:08:31 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/Module 24/m24/1/Solution.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+class Student {
+	String name;
+	double percentage;
+	Student() {
+
+	}
+	Student(String n, double p) {
+		this.name = n;
+		this.percentage = p;
+	}
+	public String getname() {
+		return this.name;
+	}
+	public double getpercentage() {
+		return this.percentage;
+	}
+}
+
+
+class Solution {
+	Solution() {
+	}
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int numberOfStudents = sc.nextInt();
+			BinarySearchST<Integer, Student> bst = new BinarySearchST<Integer, Student>();
+			int[] rollnumbers = new int[numberOfStudents];
+			String[] names = new String[numberOfStudents];
+			double[] percentages = new double[numberOfStudents];
+			for (int i = 0; i < numberOfStudents; i++) {
+				String[] data = sc.nextLine().split(",");
+				Student student = new Student();
+				student = new Student(data[1], Double.parseDouble(data[2]));
+				bst.put(Integer.parseInt(data[0]), student);
+			}
+			sc.nextLine();
+			int queries = sc.nextInt();
+			for (int i = 0; i < queries; i++) {
+				String[] data = sc.nextLine().split(" ");
+				if (data[2] == "1") {
+					System.out.println(bst.get(i).getname());
+				}
+				
+			}
+		}
+	}
