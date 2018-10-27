d319d56bc87767c1fd584f840af41228f486e1a0
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-27 11:38:36 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 24/m24/1/Solution.java
+++ Assignments/Module 24/m24/1/Solution.java
@@ -25,23 +25,20 @@ class Solution {
 			Scanner sc = new Scanner(System.in);
 			int numberOfStudents = sc.nextInt();
 			BinarySearchST<Integer, Student> bst = new BinarySearchST<Integer, Student>();
-			int[] rollnumbers = new int[numberOfStudents];
-			String[] names = new String[numberOfStudents];
-			double[] percentages = new double[numberOfStudents];
 			Student student = new Student();
 			for (int i = 0; i <= numberOfStudents; i++) {
 				String[] data = sc.next().split(",");
-				// System.out.println(data[0]);
+				System.out.println(data[0]);
 				sc.nextLine();
 				student = new Student(data[1], Double.parseDouble(data[2]));
 				bst.put(Integer.parseInt(data[0]), student);
 			}
 			String queries = sc.next();
-			for (int i = 0; i <= Integer.parseInt(queries); i++) {
+			for (int i = 0; i < Integer.parseInt(queries); i++) {
 				sc.nextLine();
 				String[] data = sc.nextLine().split(" ");
 				// System.out.println(data[0]);
-				// System.out.println(data[1]);
+				System.out.println(data[1]);
 				if (data[1].equals(null)) {
 					System.out.println("Student doesn't exists...");
 				}
