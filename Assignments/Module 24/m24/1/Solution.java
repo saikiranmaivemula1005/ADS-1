bcd9eba52913e19f593da82a5a056029d2c42f19
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-27 11:27:58 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 24/m24/1/Solution.java
+++ Assignments/Module 24/m24/1/Solution.java
@@ -38,7 +38,7 @@ class Solution {
 			}
 			int queries = sc.nextInt();
 			for (int i = 0; i < queries; i++) {
-				String[] data = sc.nextLine().split(" ");
+				String[] data = sc.next().split(" ");
 				System.out.println(data[0]);
 				System.out.println(data[1]);
 				if (data[1].equals(null)) {
