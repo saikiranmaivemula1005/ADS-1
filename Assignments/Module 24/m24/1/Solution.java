41f595b44d0871bfbbb4773151e1188e484f1a6c
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-27 12:08:30 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 24/m24/1/Solution.java
+++ Assignments/Module 24/m24/1/Solution.java
@@ -24,32 +24,30 @@ class Solution {
 		public static void main(String[] args) {
 			Scanner sc = new Scanner(System.in);
 			int numberOfStudents = sc.nextInt();
+			Student student = new Student();
 			BinarySearchST<Integer, Student> bst = new BinarySearchST<Integer, Student>();
-			while(sc.hasNext()) {
-				String data = sc.nextLine();
-				String[] tokens = data.split(",");
+			for (int i = 0; i < numberOfStudents; i++) {
+				String[] data = sc.next().split(",");
 				// System.out.println(data[1]);
+				sc.nextLine();
 				// sc.nextLine();
-				// Student student = new Student();
-				System.out.println(tokens[1]);
-				System.out.println(tokens[2]);
-				Student student = new Student(tokens[1], Double.parseDouble(tokens[2]));
-				bst.put(Integer.parseInt(tokens[0]), student);
+				student = new Student(data[1], Double.parseDouble(data[2]));
+				bst.put(Integer.parseInt(data[0]), student);
 			}
 			String queries = sc.next();
 			for (int i = 0; i < Integer.parseInt(queries); i++) {
 				sc.next();
-				String data = sc.nextLine();
-				String[] tokens = data.split(" ");
+				String[] data = sc.nextLine().split(" ");
 				// System.out.println(data[0]);
 				// System.out.println(data[2]);
-				if (tokens[1].equals(null)) {
+				if (data[1].equals(null)) {
 					System.out.println("Student doesn't exists...");
 				}
-				if (tokens[2].equals(1)) {
+				if (data[2].equals(1)) {
+					System.out.println("kk");
 					System.out.println(bst.get(i).getname());
 				}
-				if (tokens[2].equals(2)) {
+				if (data[2].equals(2)) {
 					System.out.println(bst.get(i).getpercentage());
 				}
 				
