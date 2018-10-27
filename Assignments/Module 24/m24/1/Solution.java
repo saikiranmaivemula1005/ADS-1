c55242bff9369c91940b272cb4380904e1edaf09
saikiranmaivemula1005 <saikiranmaivemula1005@msitprogram.net>
2018-10-27 13:00:23 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/Module 24/m24/1/Solution.java
+++ Assignments/Module 24/m24/1/Solution.java
@@ -1,58 +1,101 @@
+
 import java.util.Scanner;
+ /**
+ * Class for student.
+ */
 class Student {
-	String name;
-	double percentage;
-	Student() {
+     /**
+     * roll number of student.
+     */
+    private int rollnumber;
+     /**
+     * name of student.
+     */
+    private String name;
+     /**
+     * percentage of the student.
+     */
+    private double percentage;
+     /**
+     * Gets the name of the student.
+     *
+     * @return     The name.
+     *
+     */
+    String getName() {
+        return this.name;
+    }
+     /**
+     * Gets the percentage of the student.
+     *
+     * @return     The percentage.
+     */
+    double getpercentage() {
+        return this.percentage;
+    }
 
-	}
-	Student(String n, double p) {
-		this.name = n;
-		this.percentage = p;
-	}
-	public String getname() {
-		return this.name;
-	}
-	public double getpercentage() {
-		return this.percentage;
-	}
-}
+     /**
+     * Constructs the object.
+     *
+     * @param      r   rollnumber of  student
+     * @param      n      name of  student
+     * @param      p       percentage of  student
+     */
+    Student(final int r, final String n, final double p) {
+        this.rollnumber = r;
+        this.name = n;
+        this.percentage = p;
+    }
+ }
 
 
+/**
+ * Solution class.
+ */
 class Solution {
-	Solution() {
-	}
-		public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
-			int numberOfStudents = sc.nextInt();
-			sc.nextLine();
-			Student[] students = new Student[numberOfStudents];
-			BinarySearchST<Integer, Student> bst = new BinarySearchST<Integer, Student>();
-			for (int i = 0; i < numberOfStudents; i++) {
-				String[] data = sc.nextLine().split(",");
-				// System.out.println(data[1]);
-				// sc.nextLine();
-				// System.out.println(data[2]);
-				students[i] = new Student(data[1], Double.parseDouble(data[2]));
-				// System.out.println(students[i]);
-				bst.put(Integer.parseInt(data[0]), students[i]);
-			}
-			String queries = sc.next();
-			for (int i = 0; i < Integer.parseInt(queries); i++) {
-				// sc.next();
-				String[] data = sc.nextLine().split(" ");
-				// System.out.println(data[0]);
-				// System.out.println(data[2]);
-				if (data[1].equals(null)) {
-					System.out.println("Student doesn't exists...");
-				}
-				if (data[2].equals(1)) {
-					System.out.println("kk");
-					System.out.println(bst.get(i).getname());
-				}
-				if (data[2].equals(2)) {
-					System.out.println(bst.get(i).getpercentage());
-				}
-				
-			}
-		}
-	}
+     /**
+     * Constructs the object.
+     */
+    protected Solution() {
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        LinearProbingHashST<Integer, Student> lphst =
+            new LinearProbingHashST();
+        int numberOfStudents = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < numberOfStudents; i++) {
+            String[] tokens = scan.nextLine().split(",");
+            Integer rollnumber = Integer.parseInt(tokens[0]);
+            lphst.put(rollnumber,
+            new Student(rollnumber, tokens[1], Double.parseDouble(tokens[2])));
+        }
+        int queries = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < queries; i++) {
+            String[] tokens = scan.nextLine().split(" ");
+             switch (tokens[2]) {
+            case "1":
+                Student student = lphst.get(Integer.parseInt(tokens[1]));
+                if (student != null) {
+                    System.out.println(student.getName());
+                } else {
+                    System.out.println("Student doesn't exists...");
+                }
+                break;
+            case "2":
+                 student = lphst.get(Integer.parseInt(tokens[1]));
+                if (student != null) {
+                    System.out.println(student.getpercentage());
+                } else {
+                    System.out.println("Student doesn't exists...");
+                }
+                break;
+            default:
+            }
+        }
+     }
+}
